6d8bed39e2adbd1b88c93135e264b54fad2c8731
Hunter Mehring <hunterleifmehring@gmail.com>
2018-11-14 23:36:40 -0600

    final version

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -1,7 +1,9 @@
 package org.launchcode.techjobs.console;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -61,7 +63,7 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -111,6 +113,15 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
-    }
-}
+        for (int i = 0; i < someJobs.size(); i++) {
+            System.out.println("**********");
+            HashMap<String, String> tmpJobs = someJobs.get(i);
+            for(Map.Entry<String, String> job : tmpJobs.entrySet()) {
+                System.out.println(job.getKey() + ": " + job.getValue());
+            }
+        }
+        System.out.println("**********");
+
+        }
+        }
+
